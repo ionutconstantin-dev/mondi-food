4bdd2cdfbdb2b30c7b7e8093cd0ae1d2d05a890f
Ionut <ionutconstantin.dev@gmail.com>
2025-09-21 19:00:18 +0300

    Added maven files

---
+++ backend/src/main/java/com/mondi/food/MondiFoodBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mondi.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MondiFoodBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MondiFoodBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mondi/food/MondiFoodBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mondi.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MondiFoodBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
